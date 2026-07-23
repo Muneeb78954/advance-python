d8301b113115497c06e36f72290d37e5e11e417b
Muneeb Ur Rehman <f2025439020@umt.edu.pk>
2026-07-23 21:07:12 +0500

    Add timer

--- main.py
+++ main.py
@@ -12,8 +12,11 @@ print(m_names)
 m_names = [name for name in names if "M" in name]
 print(m_names)
 
+
 def avg(num1, num2):
     return (num1 + num2) / 2
+
+
 print(avg(10, 20))
 
 
@@ -24,18 +27,27 @@ print(avg_lambda(10, 20))
 """Passing function as an argument"""
 plus = lambda x, y: x + y
 
+
 def calc(x, plus):
     return x * plus(20, 10)
+
+
 print(calc(5, plus))
 
 """Anonymous function"""
+
+
 def anonymous(fx, value):
     return 10 + fx(value)
+
+
 print(anonymous(lambda x: x * x, 2))
 
-#_______________________________________________________
+
+# _______________________________________________________
 def cube(x):
     return x * x * x
+
 
 print(cube(3))
 
@@ -49,30 +61,36 @@ numbers = [23, 45, 67, 89, 12, 34, 56]
 new_list = list(map(cube, numbers))
 print(new_list)
 
-#passing lambda function to map
+# passing lambda function to map
 new_new_list = list(map(lambda x: x * x, numbers))
 print(new_new_list)
 """Filter function"""
 num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
 def filter_even(x):
     return x % 2 == 0
+
+
 even_list = list(filter(filter_even, num))
 print(even_list)
 
 """"Reduce function"""
 from functools import reduce
+
 num1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 sum = reduce(lambda x, y: x + y, num1)
 print(sum)
 
-#Typecasting str()/ int()/float()/bool()
+# Typecasting str()/ int()/float()/bool()
 num = 10
 print(type(num))
 num = str(num)
-print(type(num))    
+print(type(num))
 
-#built in math functions
+# built in math functions
 import math
+
 x = 3.14
 y = -7
 z = 10
@@ -89,7 +107,7 @@ print(math.factorial(5))
 print(math.ceil(x))
 print(math.floor(x))
 
-#ternary operator
+# ternary operator
 age = 18
 status = "Adult" if age >= 18 else "Minor"
 print(status)
---
+++ timer.py
@@ -0,0 +1,11 @@
+import time 
+
+my_time = int(input("Enter the Time in seconds: "))
+for x in range(my_time, 0, -1):
+    seconds = x % 60
+    minuts = int(x / 60) % 60
+    hours = int(x / 3600)
+    print(f"{hours:02}:{minuts:02}:{seconds:02}")
+    time.sleep(1)
+
+print("Time's UP!")
