8d839d9c97b84b296133fcb810292119a2c12327
Muneeb Ur Rehman <f2025439020@umt.edu.pk>
2026-07-26 20:44:32 +0500

    changes

--- daily.py
+++ daily.py
@@ -1,5 +1,14 @@
+# eliminate the need of number of arguments 
 def add(*args):
     total = 0
     for arg in args:
         total += arg
     return total
+print(add(2,3,4,5,6,67,7))
+
+def address(**kwargs):
+    for key , values in kwargs.items():
+        print(f"{key}: {values}")
+
+address(street = "123 LA", province ="Punjab", city ="Lahore")
+                 
