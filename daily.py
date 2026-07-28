6e6c0f714f176d2075d101989927fc3a6f910e6e
Muneeb Ur Rehman <f2025439020@umt.edu.pk>
2026-07-28 16:13:56 +0500

    classed

--- daily.py
+++ daily.py
@@ -29,3 +29,39 @@ car1.stop()
 
 car3.describe()
 
+class Student:
+    def __init__(self, sid, name, age, major, section, marks, performance):
+        self.sid         = sid
+        self.name        = name 
+        self.age         = age
+        self.major       = major
+        self.section     = section
+        self.marks       = marks
+        self.performance = performance
+    def study(self):
+        print(f"{self.name} is studying in the class!")
+
+    def play(self):
+        print(f"{self.name} is playing in the playground!")
+
+    def test(self):
+        print(f"{self.name} is in the test!")
+
+    def remarks(self):
+        print(f"{self.name} obtained {self.marks} marks. No dought {self.name} is {self.performance}")
+
+
+student1 = Student(12, 
+                   "Muneeb", 
+                   22, 
+                   "Computer Science", 
+                   "V1", 
+                   89, 
+                   "Great Student")
+
+print(student1.name)
+print(student1.marks)
+print(student1.section)
+
+student1.study()
+student1.remarks()
