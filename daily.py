73a8d8c6045662f2f4d6db5c372e384b4965c106
Muneeb Ur Rehman <f2025439020@umt.edu.pk>
2026-07-28 15:07:45 +0500

    encrypt

--- daily.py
+++ daily.py
@@ -1,5 +1,30 @@
-def main():
-    pass
+import random
+import string
 
-if __name__ == '__main__':
-    main()
+char = " " + string.punctuation + string.digits + string.ascii_letters 
+char = list(char)
+
+key = char.copy()
+random.shuffle(key)
+
+# Encryption
+plain_text = input("Enter a message to decrypt: ")
+cipher_text = " "
+
+for letter in plain_text:
+    index = char.index(letter)
+    cipher_text += key[index]
+
+print(f"Original message: {plain_text}")
+print(f"Encrypted message: {cipher_text}")
+
+# Decryption
+cipher_text = input("Enter a message to encrypt: ")
+plain_text = " "
+
+for letter in cipher_text:
+    index = key.index(letter)
+    plain_text += char[index]
+
+print(f"Encrypted message: {cipher_text}")
+print(f"Original message: {plain_text}")
