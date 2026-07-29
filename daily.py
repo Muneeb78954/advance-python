782cf72063457b87ae19380f3f33fa2952a48773
Muneeb Ur Rehman <f2025439020@umt.edu.pk>
2026-07-29 11:34:16 +0500

    Inheritance

--- daily.py
+++ daily.py
@@ -1,67 +1,28 @@
-class car:
-    def __init__(self, model, year, color, engine, speed, for_sale, price):
-        self.model = model
-        self.year = year
-        self.color = color 
-        self.engine = engine
-        self.speed = speed
-        self.for_sale = for_sale 
-        self.price = price
+class Animal:
+    def __init__(self, name, category):
+        self.name = name
+        self.category = category
+    def sound(self):
+        print("Some generic animal sound")
 
-    def drive(self):
-        print(f"You Drive The {self.color} {self.model}!")
+ #Inheritance 
+class Reptiles(Animal):
+    def sound(self):              # Polymorphism
+        print("Hissssss")
 
-    def stop(self):
-        print(f"You Stop The {self.color} {self.model}!")
+class Mammals(Animal):
+    def sound(self):              # Polymorphism
+            print("Roarrrrrr")
 
-    def describe(self):
-        print(f"{self.year} {self.color} {self.model}")
-
-car1 = car("Bughati", 2025, "Carbon Black", "V12", "235 Km/h", False, "$1.33M")
-car2 = car("Mercedes", 2026, "Silver", "V8", "195 Km/h", False, "$145K")
-car3 = car("BMW", 2026, "Red", "V8", "160 Km/h", False, "$90k")
-
-print(car1.model)
-print(car2.price)
-
-car1.drive()
-car1.stop()
-
-car3.describe()
-
-class Student:
-    def __init__(self, sid, name, age, major, section, marks, performance):
-        self.sid         = sid
-        self.name        = name 
-        self.age         = age
-        self.major       = major
-        self.section     = section
-        self.marks       = marks
-        self.performance = performance
-    def study(self):
-        print(f"{self.name} is studying in the class!")
-
-    def play(self):
-        print(f"{self.name} is playing in the playground!")
-
-    def test(self):
-        print(f"{self.name} is in the test!")
-
-    def remarks(self):
-        print(f"{self.name} obtained {self.marks} marks. No dought {self.name} is {self.performance}")
+class Birds(Animal):
+    def sound(self):              # Polymorphism
+            print("Chirppp")
 
 
-student1 = Student(12, 
-                   "Muneeb", 
-                   22, 
-                   "Computer Science", 
-                   "V1", 
-                   89, 
-                   "Great Student")
+lion = Mammals("Lion", "Mammal")
+sparrow = Birds("Sparrow", "Bird")
+snake = Reptiles("Snake", "Reptile")
 
-print(student1.name)
-print(student1.marks)
-print(student1.section)
-
-student1.study()
-student1.remarks()
+lion.sound()
+sparrow.sound()
+snake.sound()
--- main.py
+++ main.py
@@ -150,3 +150,73 @@ def day_of_week(day):
         case _:
             return "It is not a valid day!"
 day_of_week(7)
+
+
+# OOP
+class car:
+    def __init__(self, model, year, color, engine, speed, for_sale, price):
+        self.model = model
+        self.year = year
+        self.color = color 
+        self.engine = engine
+        self.speed = speed
+        self.for_sale = for_sale 
+        self.price = price
+
+    def drive(self):
+        print(f"You Drive The {self.color} {self.model}!")
+
+    def stop(self):
+        print(f"You Stop The {self.color} {self.model}!")
+
+    def describe(self):
+        print(f"{self.year} {self.color} {self.model}")
+
+car1 = car("Bughati", 2025, "Carbon Black", "V12", "235 Km/h", False, "$1.33M")
+car2 = car("Mercedes", 2026, "Silver", "V8", "195 Km/h", False, "$145K")
+car3 = car("BMW", 2026, "Red", "V8", "160 Km/h", False, "$90k")
+
+print(car1.model)
+print(car2.price)
+
+car1.drive()
+car1.stop()
+
+car3.describe()
+
+class Student:
+    def __init__(self, sid, name, age, major, section, marks, performance):
+        self.sid         = sid
+        self.name        = name 
+        self.age         = age
+        self.major       = major
+        self.section     = section
+        self.marks       = marks
+        self.performance = performance
+    def study(self):
+        print(f"{self.name} is studying in the class!")
+
+    def play(self):
+        print(f"{self.name} is playing in the playground!")
+
+    def test(self):
+        print(f"{self.name} is in the test!")
+
+    def remarks(self):
+        print(f"{self.name} obtained {self.marks} marks. No dought {self.name} is {self.performance}")
+
+
+student1 = Student(12, 
+                   "Muneeb", 
+                   22, 
+                   "Computer Science", 
+                   "V1", 
+                   89, 
+                   "Great Student")
+
+print(student1.name)
+print(student1.marks)
+print(student1.section)
+
+student1.study()
+student1.remarks()
